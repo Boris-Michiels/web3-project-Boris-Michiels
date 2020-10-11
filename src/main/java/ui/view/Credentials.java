df84ff592b63619a62131a78f39f7db1f06baa8f
Boris Michiels <boris.michiels@student.ucll.be>
2020-10-11 20:05:59 +0200

    Prepare for database implementation

---
+++ src/main/java/ui/view/Credentials.java
@@ -0,0 +1,11 @@
+package ui.view;
+
+import java.util.Properties;
+
+public abstract class Credentials {
+
+    public static void setPass(Properties dbProperties) {
+        dbProperties.setProperty("user", "local_r123456");
+        dbProperties.setProperty("password", "dagadegijniebepalen!");
+    }
+}
