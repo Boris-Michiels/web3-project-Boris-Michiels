d67f77518ba934065ab022188627083bb8f1f055
Boris Michiels <boris.michiels@student.ucll.be>
2020-12-01 09:40:31 +0100

    Update map generator

--- src/main/java/ui/controller/AdminPage.java
+++ src/main/java/ui/controller/AdminPage.java
@@ -4,9 +4,10 @@ import domain.model.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class AdminPage extends RequestHandler {
     @Override
@@ -16,10 +17,7 @@ public class AdminPage extends RequestHandler {
         List<Person> persons = getService().getAllPersons();
         List<Contact> contacts = getService().getAllContacts();
         List<TestResult> testResults = getService().getAllTestResults();
-        Map<String, Person> personMap = new HashMap<>();
-        for (Person person : persons) {
-            personMap.put(person.getUserid(), person);
-        }
+        Map<String, Person> personMap = persons.stream().collect(Collectors.toMap(Person::getUserid, Function.identity()));
         request.setAttribute("persons", persons);
         request.setAttribute("contacts", contacts);
         request.setAttribute("testResults", testResults);
